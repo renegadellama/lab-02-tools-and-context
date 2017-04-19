a0085b05e1b3915e0c8935f886cedd4c63ebadbc
Abigail White <abigailwhite213@gmail.com>
2017-04-19 08:46:48 -0700

    tests passing and mvp

--- lab-abigail/lib/lab-02.js
+++ lab-abigail/lib/lab-02.js
@@ -2,24 +2,27 @@
 
 module.exports = exports = {};
 
-exports.Dog = function(name, breed, age) {
+exports.Dog = function(name, breed, age, city) {
   this.name = name;
   this.breed = breed;
   this.age = age;
+  this.city = city;
 };
 
 //Call
-exports.WinningDog = function(name, breed, age) {
-  exports.Dog.call(this, name, breed, age);
+exports.WinningDog = function(name, breed, age, city) {
+  exports.Dog.call(this, name, breed, age, city);
+  console.log(`${name} is a ${breed}. S/he is ${age} years old and is from ${city}.`);
+  return `${name} is a ${breed}. S/he is ${age} years old and is from ${city}.`;
 };
 
-exports.winner = new exports.WinningDog('Judy', 'beagle', 20);
-console.log(exports.winner);
+exports.winner = new exports.WinningDog('Spot', 'Beagle', 4, 'Seattle');
 
 //Apply
-exports.WinningDogApply = function(name, breed, age) {
-  exports.Dog.apply(this, [name, breed, age]);
+exports.WinningDogApply = function(name, breed, age, city) {
+  exports.Dog.apply(this, [name, breed, age, city]);
+  console.log(`Congrats to ${name}! S/he is a ${breed}, ${age} years old, and is from ${city}.`);
+  return `Congrats to ${name}! S/he is a ${breed}, ${age} years old, and is from ${city}.`;
 };
 
-exports.winnerApply = new exports.WinningDogApply('Kathy', 'dalmation', 15);
-console.log(exports.winnerApply);
+exports.winnerApply = new exports.WinningDogApply('Kathy', 'Dalmation', 3 , 'Denver');
--- lab-abigail/test/lab-02-test.js
+++ lab-abigail/test/lab-02-test.js
@@ -2,3 +2,26 @@
 
 const assert = require('assert');
 const lab = require('../lib/lab-02.js');
+
+describe('lab-02.js', function() {
+  describe('#call Dog Object', function(){
+    it('should return, Spot is a Beagle. S/he is 4 years old and is from Seattle.', function() {
+      assert.equal(lab.WinningDog('Spot', 'Beagle', 4, 'Seattle'), 'Spot is a Beagle. S/he is 4 years old and is from Seattle.');
+    });
+    it('should return a string', function() {
+      assert.equal(typeof lab.WinningDog('Spot', 'Beagle', 4, 'Seattle'), 'string');
+    });
+  });
+});
+
+describe('lab-02.js', function() {
+  describe('#apply Dog Object', function(){
+    it('should return a string', function() {
+      assert.equal(typeof lab.WinningDogApply('Kathy', 'Dalmation', 3 , 'Denver'), 'string');
+    });
+
+    it('should return, Congrats to Kathy! S/he is a Dalmation, 3 years old, and is from Denver.', function() {
+      assert.equal(lab.WinningDogApply('Kathy', 'Dalmation', 3 , 'Denver'), 'Congrats to Kathy! S/he is a Dalmation, 3 years old, and is from Denver.');
+    });
+  });
+});
