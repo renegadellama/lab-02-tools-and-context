7adaaedc7b623e68a6efe95a0d6ac042aeebef64
Caleb <caleb.wells1@gmail.com>
2017-04-19 09:53:03 -0700

    some of it works

--- lab-caleb/lib/index.js
+++ lab-caleb/lib/index.js
@@ -2,30 +2,35 @@
 
 let assert = require('assert');
 
-function Product(name, price){
-  products = [];
-  addToHistory = function(){
-    Array.prototype.Push.apply(this.history, arguments);
+let products = {};
+products.gfxcard = [];
+products.keyboards = [];
+
+
+function Product(name, price, sku){
+  this.addToProducts = function(){
+    products[this.category].push;
   };
   this.product = name;
   this.pricePoint = price;
+  this.sku = sku;
   this.logProduct = function(){
     console.log(this.product);
     console.log(this.pricePoint);
-    // Product.addToHistory(this);
   };
 }
 
-function Card(name, price, architecture){
-  Product.call(this, name, price);
+function Card(name, price, sku, architecture, category){
+  Product.call(this, name, price, sku);
   this.architecture = architecture;
-  this.historyItem = function(){
-    Product.addToHistory(this);
+  this.product = function(){
+    Product.addToProducts(this);
   };
+  this.productType = category;
 }
-
-let tenEightyTi = new Card('1080ti', '$699', 'Pascal');
-let tenEighty = new Card('1080', '$399', 'Pascal');
-let tenSeventy = new Card('1070', '$299', 'Pascal');
-let tenSixty = new Card('1060', '$199', 'Pascal');
-let tenFiftyTi = new Card('1050ti', '$149', 'Pascal');
+//to test call find a way to describe it, and verify that it has the properties that the product constructor gives.
+let tenEightyTi = new Card('1080ti', '$699', 'Pascal', 'gfxcard');
+let tenEighty = new Card('1080', '$399', 'Pascal', 'gfxcard');
+let tenSeventy = new Card('1070', '$299', 'Pascal', 'gfxcard');
+let tenSixty = new Card('1060', '$199', 'Pascal', 'gfxcard');
+let tenFiftyTi = new Card('1050ti', '$149', 'Pascal', 'gfxcard');
---
+++ lab-caleb/test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let assert = require('assert');
+
+describe('products.gfxcard', function(){
+  it('should have the same property as the main products constructor', function(){
+    assert.equal(hasOwnProperty(tenEightyTi, name), true);
+  });
+});
