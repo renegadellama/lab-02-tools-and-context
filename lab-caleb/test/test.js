796a536d2b60e132f2919ed821d74f190b61654a
Caleb <caleb.wells1@gmail.com>
2017-04-19 11:37:47 -0700

    wat

--- lab-caleb/test/test.js
+++ lab-caleb/test/test.js
@@ -1,9 +1,19 @@
 'use strict';
 
 let assert = require('assert');
+const indexJS = require('../lib/index.js');
 
-describe('products.gfxcard', function(){
-  it('should have the same property as the main products constructor', function(){
-    assert.equal(hasOwnProperty(tenEightyTi, name), true);
+console.log(indexJS.products);
+console.log(indexJS.Card);
+console.log(indexJS.Product);
+
+describe('index.js', function(){
+  describe('products.gfxcard', function(){
+    it('should have the same property as the main products constructor, which is name', function(){
+      assert.equal(indexJS.products.gfxcard[0].name, '1080ti');
+    });
+    it('should show that products.gfxcard has indices', function(){
+      assert.equal(typeof indexJS.products.gfxcard[0].addToProducts, 'function');
+    });
   });
 });
