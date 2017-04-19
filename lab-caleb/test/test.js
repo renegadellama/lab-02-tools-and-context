f947d46266018b0d616c96e3d070155d5c232fc7
Caleb <caleb.wells1@gmail.com>
2017-04-19 11:54:20 -0700

    wat

--- lab-caleb/test/test.js
+++ lab-caleb/test/test.js
@@ -16,4 +16,12 @@ describe('index.js', function(){
       assert.equal(typeof indexJS.products.gfxcard[0].addToProducts, 'function');
     });
   });
+  describe('products.keyboard', function(){
+    it('should have the same property as the main products constructor, which is name', function(){
+      assert.equal(indexJS.products.keyboard[0].name, 'Black Widow');
+    });
+    it('should show that products.gfxcard has indices', function(){
+      assert.equal(typeof indexJS.products.keyboard[0].addToProducts, 'function');
+    });
+  });
 });
